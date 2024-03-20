7719ad412e9be7f4e667ee1b68fefbd46a8d8028
Sujeet Kumar Gupta <sujeet.2024ec1161@kiet.edu>
2024-03-20 20:18:17 +0530

    Partially completed

---
+++ backgroundChanger/src/backround.jsx
@@ -0,0 +1,12 @@
+
+import image from "./assets/bgimage.png";
+
+function Component() {
+    return (
+        <div style={{ backgroundImage: `url(${image})`, backgroundRepeat: "no-repeat", height: '80vh' }}>
+            
+        </div>
+    );
+}
+
+export default Component;
--- backgroundChanger/src/main.jsx
+++ backgroundChanger/src/main.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
+import './index.css';
+import image from "./assets/bgimage.png";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <div style={{ position: 'relative', overflow: 'hidden', height: '100vh' }}>
+      {/* Background Image */}
+      <div style={{
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        width: '100%',
+        height: '100%',
+        backgroundImage: `url(${image})`,
+        backgroundRepeat: 'repeat-x',
+        // backgroundSize: 'cover',
+        zIndex: -1
+      }}>
+      </div>
+
+      {/* App Component */}
+      <div style={{
+        position: 'relative', zIndex: 1, margin: '0 auto', width: '100%', maxWidth: '800px',
+        paddingTop: '100px'
+      }}>
+        <App />
+      </div>
+    </div>
+  </React.StrictMode>
+);
